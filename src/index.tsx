dff2a6affbce23d9c55bd2c25a167d58b6153605
mdrijonhossainjibon <arntoken.tem@gmail.com>
2024-10-05 16:12:24 +0600

    firs

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import { Store } from './createStore';
 import { I18nextProvider } from 'react-i18next';
 import i18n from 'i18n';
- 
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
 const root = ReactDOM.createRoot(
@@ -16,12 +16,12 @@ const root = ReactDOM.createRoot(
 root.render(
 
   <Provider store={Store}>
-   
+    <GoogleOAuthProvider clientId='109113138013-tv5kg8bh40tub6lgqi1tpm5jofvn2dis.apps.googleusercontent.com' >
 
       <I18nextProvider i18n={i18n}>
         <App />
       </I18nextProvider>
- 
+    </GoogleOAuthProvider>
 
 
   </Provider>
